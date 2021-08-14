b09963826b744e1f9d4d3eaf33fbdf3b558baa2c
powellsr <srpowell@vt.edu>
2021-08-14 19:16:12 -0400

    Color change button now working

--- js_test.js
+++ js_test.js
@@ -3,9 +3,16 @@ alert('Hello World')
 let style_button = document.getElementById('randomize-style');
 let content_button = document.getElementById('change-content');
 
-style_button.onclick(() => {
-  event.target.style.backgroundColor('rgb(' + 231 + ',' + 42 + ',' 121 + ')');
-});
+style_button.onclick = function() {
+  console.log('Hello');
+  console.log(event.target.style.backgroundColor);
+  event.target.style.backgroundColor = 'rgb(' + '231' + ',' + '42' + ',' + '121' + ')';
+};
+
+style_button.addEventListener('click', colorChange);
+//style_button.addEventHandler('click', () => {
+//  event.target.style.backgroundColor('rgb(' + '231' + ',' + '42' + ',' + '121' + ')');
+//});
 
 function colorChange(event) {
   let color = 'rgb(' + colorVal() + ',' + colorVal() + ',' + colorVal() + ')';
