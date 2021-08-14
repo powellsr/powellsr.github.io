a38fe0370af26492547d35f35ae9ee7e9c8463dc
powellsr <srpowell@vt.edu>
2021-08-14 19:00:09 -0400

    Add style-changing button

--- js_test.js
+++ js_test.js
@@ -1,1 +1,18 @@
 alert('Hello World')
+
+let style_button = document.getElementById('randomize-style');
+let content_button = document.getElementById('change-content');
+
+style_button.onclick(() => {
+  event.target.style.backgroundColor('rgb(' + 231 + ',' + 42 + ',' 121 + ')');
+});
+
+function colorChange(event) {
+  let color = 'rgb(' + colorVal() + ',' + colorVal() + ',' + colorVal() + ')';
+  let otherColor = 'rgb(' + colorVal() + ',' + colorVal() + ',' + colorVal() + ')';
+  event.target.style.backgroundColor = color;
+};
+
+function colorVal() {
+  return Math.floor(Math.random() * 256);
+};
